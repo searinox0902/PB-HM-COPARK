22f2db6879cde165d0a46d486d51aed7d164a6d9
searinox0902 <54327789+searinox0902@users.noreply.github.com>
2021-05-22 15:39:33 -0500

    Se implementa maquetacion de Admin Puestos Editar

--- main/WpfApp1/AdminPuestosEdit.xaml.cs
+++ main/WpfApp1/AdminPuestosEdit.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,68 +15,72 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for AdminPuestosEdit.xaml
-    /// </summary>
+    /// ==================== COMO SACAR UN INDEX OF DE UNA LISTA MULTIDIMENSIONAL
     public partial class AdminPuestosEdit : Window
     {
+
+        ObservableCollection<Puestos> listaPuestos = new ObservableCollection<Puestos>();
+      
+        int countPuesto = 0;
+
+ 
+
+
         public AdminPuestosEdit()
         {
             InitializeComponent();
+           
 
-            List<Puestos> listaPuestos = new List<Puestos>();
 
-            listaPuestos.Add(new Puestos() { id = "A-001", estado = false, desc = "Silla bonita", dateInit = "s", dateEnd = "s"});
+            listaPuestos.Add(new Puestos() { id = "A-001", estado = false, desc = "Silla bonita", dateInit = "s", dateEnd = "s" });
             listaPuestos.Add(new Puestos() { id = "A-002", estado = true, desc = "Silla verde", dateInit = "s", dateEnd = "s" });
             listaPuestos.Add(new Puestos() { id = "A-003", estado = false, desc = "Silla roja", dateInit = "s", dateEnd = "s" });
             listaPuestos.Add(new Puestos() { id = "A-004", estado = true, desc = "Silla cerca de la cafetera", dateInit = "s", dateEnd = "s" });
             listaPuestos.Add(new Puestos() { id = "A-005", estado = false, desc = "Con vista bonita", dateInit = "s", dateEnd = "s" });
 
+            //listaDePuestos.ItemsSource = listaPuestos;
 
             listaDePuestos.ItemsSource = listaPuestos;
 
-
-
-
+            count_puestos();
         }
 
+        // BOTON DE RETROCEDER
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
         }
 
-    
 
-        private void listaDePuestos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        // EDITAR 
+        private void Edit_Puesto(object sender, RoutedEventArgs e)
         {
-            // this.listaDePuestos.ItemsSource = 
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-           
-            if(listaDePuestos.SelectedItem != null)
-            {
-                MessageBox.Show((listaDePuestos.SelectedItem as Puestos).id);
-
-                //listaDePuestos.ItemsSource = listaPuestos;
-               
-            }
       
         }
 
+       
+
         private void seleccted_change(object sender, SelectionChangedEventArgs e)
         {
+
+            count_puestos();
+
             if (listaDePuestos.SelectedItem != null)
             {
-              //  MessageBox.Show((listaDePuestos.SelectedItem as Puestos).id);
                 PuestoId.Text = (listaDePuestos.SelectedItem as Puestos).id;
                 PuestoState.Text = Convert.ToString((listaDePuestos.SelectedItem as Puestos).estado);
-                PuestoDesc.Text = (listaDePuestos.SelectedItem as Puestos).desc;
-                //listaDePuestos.ItemsSource = listaPuestos;
-
+                PuestoDesc.Text = (listaDePuestos.SelectedItem as Puestos).desc; 
             }
         }
+
+        public void count_puestos()
+        {
+            countPuesto = listaPuestos.Count;
+            CountPuestos.Text = Convert.ToString(countPuesto);
+        }
+
+       
     }
 
     public class Puestos
@@ -86,4 +91,6 @@ namespace WpfApp1
         public string dateInit  { get; set; }
         public string dateEnd   { get; set; }
     }
+
+
 }

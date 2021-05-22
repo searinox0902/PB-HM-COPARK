c10056ec00d43dfae376ab7bad0febff76e6facb
searinox0902 <54327789+searinox0902@users.noreply.github.com>
2021-05-22 15:58:30 -0500

    Implemento maqueta de Admin Crear Usuario

---
+++ main/WpfApp1/AdminCrearPuesto.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for AdminCrearPuesto.xaml
+    /// </summary>
+    public partial class AdminCrearPuesto : Window
+    {
+        public AdminCrearPuesto()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void seleccted_change(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
